abb90269fdc8af8834dd1e5bad47e43158d9d607
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-16 14:20:23 -0500

    added skeleton logic, still working

--- RiddleGameApp/Program.cs
+++ RiddleGameApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RiddleGameApp
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RiddleGameApp
 {
     internal class Program
     {
@@ -8,8 +10,15 @@
                 "You'll be asked a random question, and have to give the word you think is the answer.\n" +
                 "You have 3 tries per question, if you fail or get through you will be told what your score was at the end.");
 
-            string[] questions = new string[]{"When you say my name I cease to exist: "};
-            //I might make a class or a method for the questions and the answer key but this is the general idea
+            string[] questions = new string[]{"When you say my name I cease to exist: ", "Poor men have me but rich men don't: "};
+            //I might make a class or a method for the questions and the answer key but this is the general idea, though I think I'll save that for the advanced version
+            //answers array has riddle answers, maybe we check if its right by if the index positions match?
+            string[] answers = new string[] {"silence", "nothing"};
+
+            foreach (string question in questions) 
+            {
+                Console.WriteLine(question);
+            }
         }
     }
 }
