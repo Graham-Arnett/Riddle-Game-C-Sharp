267e0463fad358a0475da3971de58eb252b623b2
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-15 23:36:01 -0500

    init commit, kind of tired tonight I'll do this tomorrow

---
+++ RiddleGameApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RiddleGameApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my riddle game app!\n" +
+                "You'll be asked a random question, and have to give the word you think is the answer.\n" +
+                "You have 3 tries per question, if you fail or get through you will be told what your score was at the end.");
+
+            string[] questions = new string[]{"When you say my name I cease to exist: "};
+            //I might make a class or a method for the questions and the answer key but this is the general idea
+        }
+    }
+}
