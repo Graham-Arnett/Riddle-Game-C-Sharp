dfb5aa6aa1dee80c30fd464dab1f04bb7bedb0a0
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-19 21:04:23 -0500

    added counting for score and the do/while loop

--- RiddleGameApp/Program.cs
+++ RiddleGameApp/Program.cs
@@ -9,26 +9,33 @@ namespace RiddleGameApp
         {
             Console.WriteLine("Welcome to my riddle game app!\n" +
                 "You'll be asked a random question, and have to give the word you think is the answer.\n" +
-                "You have 3 tries per question, if you fail or get through you will be told what your score was at the end.");
-  
+                "You have 3 tries per question, if you fail or get through you will be told what your score was at the end. Note all answers are a single word.");
+            string choice;
+            int score = 0;
             string[] questions = new string[]{"When you say my name I cease to exist: ", "Poor men have me but rich men don't: "};
             //I might make a class or a method for the questions and the answer key but this is the general idea, though I think I'll save that for the advanced version
             //answers array has riddle answers, maybe we check if its right by if the index positions match?
             string[] answers = new string[] {"silence", "nothing"};
-
-            foreach (string question in questions) 
+            do
             {
-                Console.Write(question);
-                string answer = Console.ReadLine();
-                if (answers.Contains(answer))
+                foreach (string question in questions)
                 {
-                    Console.WriteLine("Correct!");
+                    Console.Write(question);
+                    string answer = Console.ReadLine();
+                    if (answers.Contains(answer))
+                    {
+                        Console.WriteLine("Correct!");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was WRONG!");
+                    }
                 }
-                else 
-                {
-                    Console.WriteLine("That was WRONG!");
-                }
-            }
+                Console.WriteLine($"You ended up with a score of: {score}");
+                Console.WriteLine("Would you like to try again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
         }
     }
 }
