79a7c49e6c6479d5a47cd6cd5d6014e8711c1584
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-20 19:00:32 -0500

    still working on it, fixed a bug in the score, need to see how I can link a specific answer to a question

--- RiddleGameApp/Program.cs
+++ RiddleGameApp/Program.cs
@@ -11,13 +11,14 @@ namespace RiddleGameApp
                 "You'll be asked a random question, and have to give the word you think is the answer.\n" +
                 "You have 3 tries per question, if you fail or get through you will be told what your score was at the end. Note all answers are a single word.");
             string choice;
-            int score = 0;
+            
             string[] questions = new string[]{"When you say my name I cease to exist: ", "Poor men have me but rich men don't: "};
             //I might make a class or a method for the questions and the answer key but this is the general idea, though I think I'll save that for the advanced version
             //answers array has riddle answers, maybe we check if its right by if the index positions match?
             string[] answers = new string[] {"silence", "nothing"};
             do
             {
+                int score = 0;
                 foreach (string question in questions)
                 {
                     Console.Write(question);
