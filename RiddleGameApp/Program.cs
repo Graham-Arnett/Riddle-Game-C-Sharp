0fa359dfc7d5c76cf8054846e2d2c37514a42937
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-22 22:15:04 -0500

    added a new riddle, I would have done more but I was really busy today

--- RiddleGameApp/Program.cs
+++ RiddleGameApp/Program.cs
@@ -12,10 +12,10 @@ namespace RiddleGameApp
                 "You have 3 tries per question, if you fail or get through you will be told what your score was at the end. Note all answers are a single word.");
             string choice;
             
-            string[] questions = new string[]{"When you say my name I cease to exist: ", "Poor men have me but rich men don't: "};
+            string[] questions = new string[]{"When you say my name I cease to exist: ", "Poor men have me but rich men don't: ","I have a face and a tail, but no body: "};
             //I might make a class or a method for the questions and the answer key but this is the general idea, though I think I'll save that for the advanced version
             //answers array has riddle answers, maybe we check if its right by if the index positions match?
-            string[] answers = new string[] {"silence", "nothing"};
+            string[] answers = new string[] {"silence", "nothing","coin"};
             do
             {
                 int score = 0;
