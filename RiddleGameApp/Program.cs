63dc20b710dc755eeee1b8a5de05c93c5ee2c816
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-06 20:32:19 -0500

    idk anymore

--- RiddleGameApp/Program.cs
+++ RiddleGameApp/Program.cs
@@ -16,7 +16,7 @@ namespace RiddleGameApp
           
             string[] answers = new string[] {"silence", "nothing","coin", "glove"};
 
-            string[] hints = new string[] {"Not loud", "The opposite of everything", "A currency made of metal","A clothing item that you wear during the winter"};
+            string[] hints = new string[] {"Very quiet", "The opposite of everything", "A currency made of metal","A clothing item that you wear during the winter"};
             do
             {
                 int score = 0;
